cd73d2d87aa7b9e86addd9df112aa393666a3d2e
mt667 <81780996+mt667@users.noreply.github.com>
2022-04-29 23:45:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,14 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+st.set_page_config(
+     page_title="VroomVroom",
+     page_icon="🧊",
+     layout="centered",
+     initial_sidebar_state="auto",
+     menu_items=None
+ )
+
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
     option = st.selectbox(
@@ -36,31 +44,24 @@ with right_column:
 
 right_column.button('Confirm!')
 
+with left_column.container():
+    st.header('Best Price')
+    st.write("Sell your car at the best price no matter how long it takes.")
+    st.button('Get Best Price')
+    # You can call any Streamlit command, including custom components:
 
-st.button('Get price')
+with middle_column.container():
+    st.header('No Preference')
+    st.write("You cannot wait forever, but you can wait for an offer that is right for you.")
+    st.button('Get Price for No Preference')
+    # You can call any Streamlit command, including custom components:    
+
+with right_column.container():
+    st.header('Fast Sale')
+    st.write("Sell your car as fast as possible, likely through a dealer or broker.")
+    st.button('Get Fast Sale Price')
+    # You can call any Streamlit command, including custom components:    
 
 with st.spinner('Wait for it...'):
     time.sleep(5)
 st.success('Done!')
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
