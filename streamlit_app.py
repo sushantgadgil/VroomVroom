89f2741d7ae0a292b54494c420a3addbcefd4cee
sushantgadgil <98911787+sushantgadgil@users.noreply.github.com>
2022-05-04 16:17:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,24 +33,24 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 #Leave uncommented to test from Google Drive File
 
-gDrivepath = 'https://drive.google.com/file/d/1NeAmUoe9FqYsEAW8vHlRcjlKF_r7b3Ou/view?usp=sharing'
-gDrivepath='https://drive.google.com/uc?id=' + gDrivepath.split('/')[-2]
+#gDrivepath = 'https://drive.google.com/file/d/1NeAmUoe9FqYsEAW8vHlRcjlKF_r7b3Ou/view?usp=sharing'
+#gDrivepath='https://drive.google.com/uc?id=' + gDrivepath.split('/')[-2]
 
-@st.cache(persist=True)
-def download(path):
-    gdown.download(url=path, output='used_cars_dataset_trimmed.csv', quiet=False)
-    df = pd.read_csv('used_cars_dataset_trimmed.csv')
-    return df
+#@st.cache(persist=True)
+#def download(path):
+    #gdown.download(url=path, output='used_cars_dataset_trimmed.csv', quiet=False)
+    #df = pd.read_csv('used_cars_dataset_trimmed.csv')
+    #return df
 
-df = download(gDrivepath)
+#df = download(gDrivepath)
 
 
 #Local Cache
 
 # To test from local cache, uncomment both following lines.
 
-# localPath = '~/Documents/GitHub/VroomVroom/Dataset/used_cars_dataset_trimmed.csv' #Edit this line with the filepath of the .csv
-# df = pd.read_csv(localPath) #Read in CSV
+localPath = '~/Documents/GitHub/VroomVroom/Dataset/used_cars_dataset_trimmed.csv' #EDIT this line with the filepath of the .csv to stream locally
+df = pd.read_csv(localPath) #Read in CSV
 
 #Create Dropdown Menu
 
